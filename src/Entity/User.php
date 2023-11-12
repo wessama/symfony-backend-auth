85fdc9aa48aefb289d3077d654002344850d464e
Wessam-Ahmed <wessam.ah@outlook.com>
2023-11-12 15:28:36 +0200

    Ensure e-mail is unique

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -6,12 +6,17 @@ use App\Repository\UserRepository;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
 use Symfony\Component\Security\Core\User\UserInterface;
 use Symfony\Component\Validator\Constraints as Assert;
 
 /**
  * @ORM\Entity(repositoryClass=UserRepository::class)
  * @ORM\HasLifecycleCallbacks()
+ * @UniqueEntity(
+ *      fields={"email"},
+ *      message="This email is already in use."
+ *  )
  */
 class User extends BaseEntity implements UserInterface
 {
