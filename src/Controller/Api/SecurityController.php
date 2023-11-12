ee0928eb292077ac61f9de55e320d30d9aeae7e9
Wessam-Ahmed <wessam.ah@outlook.com>
2023-11-12 15:55:47 +0200

    Dummy controller route for login

---
+++ src/Controller/Api/SecurityController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Controller\Api;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class SecurityController extends AbstractController
+{
+    /**
+     * @Route("/login", name="login", methods={"POST"})
+     */
+    public function login()
+    {
+        // Intercepted by the firewall
+    }
+}
