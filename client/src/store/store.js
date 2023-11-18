2710b966f222f813ba14e199662ecef7fd6e25e5
Wessam-Ahmed <wessam.ah@outlook.com>
2023-11-18 13:34:54 +0200

    Minor

--- client/src/store/store.js
+++ client/src/store/store.js
@@ -10,7 +10,7 @@ const rootReducer = combineReducers({
 
 const persistedState = {
     auth: {
-        token: localStorage.getItem('jwtToken')
+        token: localStorage.getItem('jwtToken') || null
     }
 };
 
