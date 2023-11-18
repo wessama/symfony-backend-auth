780bdee210f399d4c17e3f92a3406d3c99403512
Wessam-Ahmed <wessam.ah@outlook.com>
2023-11-18 13:25:07 +0200

    Revert "Remove redundant variable"
    This reverts commit a5bc498d0360703a3b106bf22d4840400bae77be.

--- client/src/components/utils/AuthGuard.js
+++ client/src/components/utils/AuthGuard.js
@@ -1,8 +1,9 @@
-import {useSelector} from 'react-redux';
-import {Navigate} from 'react-router-dom';
+import { useSelector } from 'react-redux';
+import { Navigate } from 'react-router-dom';
 
 const AuthGuard = ({ children, type }) => {
-    const isAuthenticated = useSelector(state => state.auth.token);
+    const token = useSelector(state => state.auth.token);
+    const isAuthenticated = !!token;
 
     if (type === 'guest' && isAuthenticated) {
         return <Navigate to="/profile" />;
