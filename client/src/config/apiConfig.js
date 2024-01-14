761b75236e8afdeae496011d5887269321f05f93
Wessam-Ahmed <wessam.ah@outlook.com>
2024-01-14 15:29:33 +0200

    Update API url

--- client/src/config/apiConfig.js
+++ client/src/config/apiConfig.js
@@ -3,5 +3,5 @@ const API_ROOT = process.env.REACT_APP_API_URL;
 export const ENDPOINTS = {
     REGISTER: `${API_ROOT}/users/register`,
     LOGIN: `${API_ROOT}/users/login`,
-    ME: `${API_ROOT}/me`,
+    ME: `${API_ROOT}/users/me`,
 };
